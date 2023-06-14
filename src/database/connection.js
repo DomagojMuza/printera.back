b2063cfe79867d8f2a4a3d3eb68cac9e8cd3f04a
Domagoj <domim1998@gmail.com>
2023-06-14 17:30:29 +0200

    db link

--- src/database/connection.js
+++ src/database/connection.js
@@ -2,6 +2,6 @@
 const mongoose = require('mongoose');
 
 mongoose.set('strictQuery', true);
-mongoose.connect( 'mongodb://127.0.0.1:27017/bookistra')
+mongoose.connect( 'mongodb+srv://domim1998:jasamzakon98@bookistra.xakpyv4.mongodb.net/?retryWrites=true&w=majority')
 .then(() => console.log( 'Database Connected' ))
 .catch(err => console.log( err ));
